f365411bf1885d53229c9b15ea92f77e153b46b7
hihimamuLab <hihimamu14@outlook.jp>
2023-06-26 23:54:13 +0900

    Add: code in ./after/plugin/keymap.lua

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -7,3 +7,6 @@ keymap.set("i", "(", "()<LEFT>")
 keymap.set("i", [[']], [[''<LEFT>]])
 keymap.set("i", "[", "[]<LEFT>")
 keymap.set("i", "<D-s>", ":w")
+keymap.set('n', '<C-Right>', ':TablineBufferNext<CR>', { silent = true })
+keymap.set('n', '<C-Left>', ':TablineBufferPrevious<CR>', { silent = true })
+
