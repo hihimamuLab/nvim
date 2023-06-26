6d20fdfef80443ce30324d2dffc46f32996fabba
hihimamuLab <122292089+hihimamuLab@users.noreply.github.com>
2023-06-26 16:36:06 +0900

    Add: vim.opt.swapfile = false

--- lua/opt.lua
+++ lua/opt.lua
@@ -5,11 +5,11 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 vim.opt.ruler = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.title = true
 vim.opt.shell = 'zsh'
 vim.opt.cmdheight = 1
 vim.opt.showcmd = true
 vim.opt.hlsearch = true
-
+vim.opt.swapfile = false
